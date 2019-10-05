0b41a2c1d58f140f4044e3c52aadb246d33e7d7f
Cornelia <cornelia.persson413@gmail.com>
2019-10-05 18:04:10 +0200

    lagt till network access

--- index.js
+++ index.js
@@ -5,24 +5,36 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 let db;
 const com = 'comments';
+
+
 //to get my css running:
 app.use(express.static(__dirname))
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 //to create a server where browsers can connect to, use the listen method provided by express
+/*
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
+*/
 
 //to answer the get request from the browser we use the get method  
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+ '/index.html')
+    //res.sendFile(__dirname+ '/index.html')
+    var cursor = db.collection(com).find().toArray(function(err, results){
+        console.log(results)
+        if(err){
+            console.log(err)
+    
+        }
+
+    })
 })
 
 app.post('/comments', (req, res) => {
     db.collection(com).save(req.body, (err, result) => {
-        if (err) return console.log(err)
+        if (err) return console.log(err);
     
         console.log('saved to database')
         res.redirect('/')
@@ -32,9 +44,13 @@ app.post('/comments', (req, res) => {
 
 
 MongoClient.connect('mongodb+srv://nellantestar:karamell1@cluster0-o6sra.mongodb.net/admin?retryWrites=true&w=majority', (err, database) => {
-    if (err) return console.log(err)
-    db = client.db('Cluster0') // whatever your database name is
-    app.listen(3000, () => {
-      console.log('listening on 3000')
-    })
+    if(err){
+        console.log(err);
+    } else {
+        db = database.db('Cluster0');
+        app.listen(3000, () => {
+            console.log("lyssnar på port 3000");
+            
+        });
+    };
 })
